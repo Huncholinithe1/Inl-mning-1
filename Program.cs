d55b799b43f52c95a63dc69e01e479e3f50866d8
Huncholinithe1 <yousef.raooof@gmail.com>
2024-08-22 22:59:06 +0200

    Added comments

--- Program.cs
+++ Program.cs
@@ -11,14 +11,14 @@ namespace Inlämning_1
         static void Main(string[] args)
         {
             Console.WriteLine("Hej och välkommen till IT-Högskolan!, skriv in ditt namn nedan.");
-            String name = Console.ReadLine();
+            String name = Console.ReadLine(); // string variabeln andvänds för text 
             Console.WriteLine($" Hej {name}");
             Console.WriteLine("Skriv sedan in din ålder så ska jag räkna ut vilket år du är född :)");
             string input = Console.ReadLine();
-            int age = int.Parse(input);
+            int age = int.Parse(input); // int används för heltals siffor endast. double andvänds för både och
             int birthyear = 2024 - age;
             Console.WriteLine($"Du är {age} år gammal och du är född år {birthyear}");
-            Console.WriteLine("Tryck på valfri kanpp för att stänga detta fönster. Adjö:)");
+            Console.WriteLine("Tryck på valfri kanpp för att stänga detta fönster. Adjö:)"); // variabeln skriver ut allt inom ()
             Console.ReadKey();
         }
     }
